89a439d1396ae0e533a486e813723db716f6dbcf
Konstantsin Fedarakin <kostyafedorakin@gmail.com>
2022-05-14 21:44:51 +0300

    A simple example is implemented

---
+++ Recive/HelloWorld/Recive.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Recive.HelloWorld;
+
+internal class Recive
+{
+    private ConnectionFactory connectionFactory;
+
+    public Recive(string hostName = "127.0.0.1", int port = 5672)
+    {
+        connectionFactory = new ConnectionFactory() { HostName = hostName, Port = port };
+    }
+
+    public string Queue { get; init; } = "hello";
+
+    public void ReciveMessage()
+    {
+        using (var connection = connectionFactory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: Queue,
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, args) =>
+            {
+                var body = args.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+
+            channel.BasicConsume(queue: Queue,
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Send/HelloWorld/Send.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Send.HelloWorld;
+
+internal class Send
+{
+    private ConnectionFactory connectionFactory;
+
+    public Send(string hostName = "127.0.0.1", int port = 5672)
+    {
+        connectionFactory = new ConnectionFactory() { HostName = hostName, Port = port, UserName = "guest", Password = "guest"};
+    }
+
+    public string Queue { get; init; } = "hello";
+
+    public void SendMessage(string message = "Hello World!")
+    {
+        using (var connection = connectionFactory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: Queue,
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: string.Empty,
+                                 routingKey: Queue,
+                                 basicProperties: null,
+                                 body: body);
+
+            Console.WriteLine("Message sended");
+        }
+    }
+}
